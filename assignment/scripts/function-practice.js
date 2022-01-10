464ea0201361ffa782db9588002b8f7820cfab39
Vince Saykao <vincesaykao@Vinces-MacBook-Air.local>
2022-01-09 22:16:06 -0600

    9 stretch attempt

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -108,11 +108,20 @@ console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+
+/*arrayNumber = [2,4,6,-10]
+
+function sumAll(totalNumber) {
   let sum = 0
+  totalNumber = (arrayNumber[i] + )
+  for (let i=0;i<arrayNumber.length;i++) {
+    return (totalNumber)
+  }
   // TODO: loop to add items
   return sum;
 }
+
+*/
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
